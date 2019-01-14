c70732a5c2413f0780e36e07fde9c9bb24ec6905
Alex I <alex.tutmee@gmail.com>
2019-01-14 17:36:09 +0300

    add redirect for not logged in users

--- privatemessages/urls.py
+++ privatemessages/urls.py
@@ -1,14 +1,11 @@
-# from django.conf.urls import patterns, url
-from django.urls import path, include, re_path
+from django.urls import include, re_path
 
 from . import views
 
 urlpatterns = [
-    # path('privatemessages.views',
     re_path(r'send_message/$', views.send_message_view, name='send_message_view'),
     re_path(r'send_message_api/(?P<thread_id>\d+)/$', views.send_message_api_view, name='send_message_api_view'),
     re_path(r'chat/(?P<thread_id>\d+)/$', views.chat_view, name='chat_view'),
-    re_path(r'^accounts/', include('django.contrib.auth.urls')),
-    re_path(r'$', views.messages_view, name='messages_view'),
-    # )
+    re_path(r'accounts/', include('django.contrib.auth.urls')),
+    re_path(r'', views.messages_view, name='messages_view'),
 ]
